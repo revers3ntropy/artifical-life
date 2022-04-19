8635c84f2d10cc637cb933bc1fadfd08444b4a5b
Joseph Coppin <josephcoppin@gmail.com>
2022-04-19 21:32:54 +0100

    start of node backend

--- src/sim/component.rs
+++
@@ -1,4 +0,0 @@
-pub trait Component {
-    fn init (&self, &e: Entity);
-    fn update (&self, &e: Entity);
-}
--- src/sim/entity.rs
+++
@@ -1,6 +0,0 @@
-use serde::Serialize;
-
-pub trait Entity : Serialize {
-    fn init (&self);
-    fn update (&self, delta_t: u64);
-}
